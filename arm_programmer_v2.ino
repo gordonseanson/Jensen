4840f23fbe4c1e43b15da2430cca63b8d005f3e3
seanjohnsonmakes <8bandcomp@gmail.com>
2019-01-06 16:12:50 -0500

    refactored

--- arm_programmer_v2.ino
+++ arm_programmer_v2.ino
@@ -3,18 +3,8 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-// Pin numbers
-int SAVE = 37;
-float PROGRAM = 35;
-float PLAYPAUSE = 33;
-// Delay between each while() loop
-float delay_us = 1;
-// Error margin for each move
-int errorMargin = 4;
-// Number of frames with moves
-float actualFrames;
-
 // Array for saving each position
+// 6 servo motors and 100 possible arm positions
 int frames = 100;
 int positionArray[100][6] = {
   {0, 0, 0, 0, 0, 0},
@@ -118,62 +108,103 @@ int positionArray[100][6] = {
   {0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0}
 };
+// Number of frames with non-zero data
+float actualFrames;
 
-//enhancedServo::enhancedServo(
+int delayFrames[100] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+// Constructor from enhancedServo library
+
+// enhancedServo::enhancedServo(
 //  Adafruit_PWMServoDriver _pwm, 
 //  0  String _name, 
-//  1  uint8_t _feed_pin, 
-//  2  uint8_t _pot_pin,
-//  3  uint8_t _pot_reference_pin, 
-//  4  uint8_t _write_pin, 
+//  1  uint8_t _feed_pin, // middle pin of potentiometer
+//  2  uint8_t _pot_pin,  // top pin of potentiometer
+//  3  uint8_t _pot_reference_pin,  // bottom pin of potentiometer
+//  4  uint8_t _write_pin,  // write pin of servo
 //  5  double _write_min, 
 //  6  double _write_max, 
 //  7  int _feed_min,
 //  8  int _feed_max,
-//  9  double _Ki, 
-//  10 double _alpha, 
-//  11 double _errorMargin) {
-//                           0          1    2    3    4  5    6    7    8    9      10     11
-enhancedServo base(pwm,     "Base",     A0,  A1,  A2,  0, 135, 500, 127, 860, 0.005, 0.02, 3, false);
+//  9  double _Ki,  // constant for tuning integral control loop
+//  10 double _alpha, // constant for tuning control loop EWMA
+//  11 double _errorMargin
+//  12 boolean gripper
+// ) {
+
+// Error margin for each move
+int errorMargin = 4;
+
+// Initalizing each servo motor
+//                           0          1    2    3    4  5    6    7    8    9      10    11           12
+enhancedServo base(pwm,     "Base",     A0,  A1,  A2,  0, 135, 500, 127, 860, 0.005, 0.02, 3,           false);
 enhancedServo shoulder(pwm, "Shoulder", A3,  A4,  A5,  1, 170, 500, 158, 807, 0.005, 0.02, errorMargin, false);
 enhancedServo elbow(pwm,    "Elbow",    A6,  A7,  A8,  2, 150, 470, 140, 770, 0.005, 0.02, errorMargin, false);
 enhancedServo wrist(pwm,    "Wrist",    A9,  A10, A11, 3, 135, 500, 118, 844, 0.005, 0.02, errorMargin, false);
 enhancedServo rotation(pwm, "Rotation", A12, A13, A14, 4, 150, 450, 117, 965, 0.005, 0.02, errorMargin, false);
-enhancedServo gripper(pwm,  "Gripper",  A15, A13, A14, 5, 290, 390, 248, 332, 0.01, 0.02, 13, true);
+enhancedServo gripper(pwm,  "Gripper",  A15, A13, A14, 5, 290, 390, 248, 332, 0.01,  0.02, 13,          true);
+
+// Delay between each while() loop
+float delay_us = 1;
+
+
+// Debugging flags
+bool programming_DEBUG = false;
+bool playback_DEBUG = false;
+
+// Helpful bools
+bool isDefault = false;
+bool isProgrammed = false;
+
+// Pin numbers
+int SAVE = 37;
+int PROGRAM = 35;
+int PLAYPAUSE = 33;
+int HOME = 31;
 
 void setup() {
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  
-  Serial.begin(2000000);
+  Serial.begin(250000);
   pinMode(SAVE, INPUT);
   pinMode(PROGRAM, INPUT);
   pinMode(PLAYPAUSE, INPUT);
+  // Initialize feedback running averages and move to default position
   setupInitializer();
-  preInitializer();
-  loopInitializer();
-  while(!isArrived()) {
-      base.computePath(480);
-      shoulder.computePath(740);
-      elbow.computePath(490);
-      wrist.computePath(140);
-      rotation.computePath(585);
-      gripper.computePath(322);
-      delayMicroseconds(delay_us);
-  }
+  defaultPos();
 }
 
 // Main loop; can select various methods with the physical buttons
 void loop() {
-  // Skips method without these loops
-  while (digitalRead(PROGRAM) == HIGH) {
-  }
-  while (digitalRead(SAVE) == HIGH) {
-  }
-  while (digitalRead(PLAYPAUSE) == HIGH) {
-  }
-  // Beginning message and branching logic depending on button pressed
-  Serial.print("Standby!\n");
+  // Debouncing
+  while (digitalRead(PROGRAM) == HIGH) {}
+  while (digitalRead(SAVE) == HIGH) {}
+  while (digitalRead(PLAYPAUSE) == HIGH) {}
+  while (digitalRead(HOME) == HIGH) {}
+
+  // Main menu
+  Serial.println("\nMain menu:");
+  
+  Serial.println(" Program - Give the arm a set of positions to cycle through.");
+  
+  if (isProgrammed) Serial.println(" Play - Begin movement.");
+  else Serial.println(" Play - Must program first!");
+  
+  if (!isDefault) Serial.println(" Home - Move the arm to its default position.");
+  else Serial.println(" Home - The arm is already in its default position.");
+  
+  // Branching logic depending on button pressed
   char button = 'x';
   bool standby = true;
   while (standby) {
@@ -186,25 +217,219 @@ void loop() {
       button = 'p';
       standby = false;
     }
+    if (digitalRead(HOME) == HIGH) {
+      button = 'h';
+      standby = false;
+    }
   }
+
+  // Switch case for selected button
   switch (button) {
     case 'c':
-      Serial.print("Programming Mode!\n");
+      Serial.println("\nProgramming Mode");
+      Serial.println("Use the save button to save a sequence of positions.");
+      Serial.println("Use the play/pause button to insert a 1 second delay.");
       program();
       break;
     case 'p':
-      Serial.print("Playback Mode!\n");
-      while(digitalRead(PLAYPAUSE) == HIGH) {
+      Serial.println("\nPlayback Mode");
+      // Debouncing
+      while (digitalRead(PLAYPAUSE) == HIGH) {}
+      
+      while (digitalRead(PLAYPAUSE) == LOW && digitalRead(HOME) == LOW) {
+        playback();
       }
-      while(digitalRead(PLAYPAUSE) == LOW) {
-      playback();
-      }
+      
+      if (digitalRead(HOME) == HIGH) defaultPos();
+      break;
+    case 'h':
+      defaultPos();
       break;
     default:
       break;
   }
 }
 
+// Playback mode: cycles through saved arm positions
+void playback() {
+  // Debouncing
+  while (digitalRead(PLAYPAUSE) == HIGH) {}
+  
+  // Count number of arm positions
+  actualFrames = 0;
+  for (int i = 0; i < frames; i++) {
+    if (positionArray[i][0] != 0) {
+      actualFrames++;
+    }
+  }
+
+  preInitializer(); // Initializes the integral control loop
+  boolean playing = true;
+  // Each for() loop represents one "move" to a arm position
+  for (int i = 0; i < actualFrames; i++) {
+    // Initializes the arrival conditions as false before each move
+    loopInitializer();
+    // While the arrival conditions are false, calculates error correction output for each servo
+    while(!isArrived()) {
+      // Checks if pause button has been pressed
+      if (digitalRead(PLAYPAUSE) == HIGH || digitalRead(HOME) == HIGH) {
+        playing = false;
+        break;
+      }
+
+      if (delayFrames[i] == 1) {
+        delay(1000);
+        break;
+      }
+      
+      // Continually looping computePath() moves the arm (see enhancedServo library)
+      computePath(i);
+      isDefault = false;
+      delayMicroseconds(delay_us);
+    }
+    if (!playing) break;
+    if (delayFrames[i] == 0) Serial.print("Move "); 
+    else Serial.print("Delay: ");
+    Serial.print(i+1); Serial.print(" completed"); Serial.print("\n");
+  }
+}
+
+// "Programs" the robot arm by saving an array of positions. Each position is a "frame."
+void program() {
+  // Debouncing
+  while (digitalRead(PROGRAM) == HIGH) {}
+  
+  // Set all frames to 0
+  for (int i = 0; i < frames; i++) {
+    for (int j = 0; j < 2; j++) {
+      positionArray[i][j] = 0;
+    }
+    delayFrames[i] = 0;
+  }
+  
+  // Assigns values to the array in sequential order
+  boolean programming_mode = true;
+  detach(); // Allows you to move the servos freely with your hand
+
+  // In each iteration of the for() loop, one position of the arm is saved to the position array
+  for (int i = 0; i < frames; i++) {
+    //  Exits programming mode when the "Program" button is pressed again
+    if (programming_mode == false) {
+      break;
+    }
+
+    // Debouncing
+    while (programming_mode && (digitalRead(SAVE) == HIGH || digitalRead(PLAYPAUSE) == HIGH)) {};
+
+    //  Waits until "Save" or "Program" is pressed
+    bool waiting = true;
+    while (waiting) {
+      feedbackRefresh();  // Keeps running averages for feedback going
+
+      // Debug print statements
+      if (programming_DEBUG) {
+        Serial.print(base.getFeedback()); Serial.print(" ");
+        Serial.print(shoulder.getFeedback()); Serial.print(" ");
+        Serial.print(elbow.getFeedback()); Serial.print(" ");
+        Serial.print(wrist.getFeedback()); Serial.print(" ");
+        Serial.print(rotation.getFeedback()); Serial.print(" ");
+        Serial.print(gripper.getFeedback()); Serial.println(" ");
+      }
+
+      // If "Program" is pressed while in programming mode, the positions are saved and programming mode is exited
+      if (digitalRead(PROGRAM) == HIGH) {
+        programming_mode = false;
+        waiting = false;
+      }
+
+      // If "Save" is pressed, each angle between the links of the arm are saved to one row of the position array
+      if (digitalRead(SAVE) == HIGH) {
+        savePositions(i);
+        Serial.print("Position saved ["); Serial.print(i+1); Serial.println("/100]");
+        waiting = false;
+      }
+
+      // If "Stop" is pressed, a delay will be inserted into the sequence of motion
+      if (digitalRead(PLAYPAUSE) == HIGH) {
+        savePositions(i); // Just saving the positions so that they aren't zero (data will not be used)
+        delayFrames[i] = 1;
+        Serial.print("Delay saved ["); Serial.print(i+1); Serial.println("/100]");
+        waiting = false;
+      }
+
+      delay(200);
+    }
+  }
+
+  isProgrammed = true;
+  Serial.print("Program Saved\n");
+  
+  hold(); // holds arm position after programming is done
+  
+  // Count number of frames
+  float actualFrames = 0;
+  for (int i = 0; i < frames; i++) {
+    if (positionArray[i][0] != 0) {
+      actualFrames++;
+    }
+  }
+
+  // Prints saved positions
+  if (programming_DEBUG) {
+    for (int i = 0; i < actualFrames; i++) {
+      Serial.print("Position "); Serial.print(i + 1); 
+      Serial.print(" feedback: "); 
+      Serial.print(positionArray[i][0]); Serial.print(" "); 
+      Serial.print(positionArray[i][1]); Serial.print(" "); 
+      Serial.print(positionArray[i][2]); Serial.print(" "); 
+      Serial.print(positionArray[i][3]); Serial.print(" ");
+      Serial.print(positionArray[i][4]); Serial.print(" ");
+      Serial.println(positionArray[i][5]);
+    }
+  }
+}
+
+// All functions below group servo actions
+
+// Puts arm in default position
+void defaultPos() {
+  Serial.println("\nMoving to default position");
+  preInitializer();
+  loopInitializer();
+  while(!isArrived()) {
+      base.computePath(480);
+      shoulder.computePath(740);
+      elbow.computePath(490);
+      wrist.computePath(140);
+      rotation.computePath(585);
+      gripper.computePath(322);
+      delayMicroseconds(delay_us);
+  }
+  isDefault = true;
+}
+
+// Holds arm position
+void hold() {
+  int baseF = base.getFeedback();
+  int shoulderF = shoulder.getFeedback();
+  int elbowF = elbow.getFeedback();
+  int wristF = wrist.getFeedback();
+  int rotationF = rotation.getFeedback();
+  int gripperF = gripper.getFeedback();
+  preInitializer();
+  loopInitializer();
+  while(!isArrived()) {
+      base.computePath(baseF);
+      shoulder.computePath(shoulderF);
+      elbow.computePath(elbowF);
+      wrist.computePath(wristF);
+      rotation.computePath(rotationF);
+      gripper.computePath(gripperF);
+      delayMicroseconds(delay_us);
+  }
+}
+
+// Call this function before doing any arm movement to correctly initialize running averages for feedback
 void setupInitializer() {
   base.setupInitializer();
   shoulder.setupInitializer();
@@ -212,16 +437,6 @@ void setupInitializer() {
   wrist.setupInitializer();
   rotation.setupInitializer();
   gripper.setupInitializer();
-}
-
-// Delegates all servo attachments
-void attach() {
-  base.attach();
-  shoulder.attach();
-  elbow.attach();
-  wrist.attach();
-  rotation.attach();
-  gripper.attach();
 }
 
 // Delegates all servo detachments
@@ -274,6 +489,8 @@ void savePositions(int i) {
   positionArray[i][5] = gripper.getFeedback();
 }
 
+// Once the arm starts, all feedback is kept in running averages
+// This function is called in programming mode to keep the averages going
 void feedbackRefresh() {
   base.getFeedback();
   shoulder.getFeedback();
@@ -285,13 +502,14 @@ void feedbackRefresh() {
 
 // Returns true if all servos have arrived at their respective positions
 boolean isArrived() {
-  if (true) {
-//    Serial.print(base.getArrived()); Serial.print(" "); Serial.print(base.getFeedback()); Serial.print(" ");
-//    Serial.print(shoulder.getArrived()); Serial.print(" ");
-//    Serial.print(elbow.getArrived()); Serial.print(" ");
-//    Serial.print(wrist.getArrived()); Serial.print(" ");
-//    Serial.print(rotation.getArrived()); Serial.print(" ");
-//    Serial.print(gripper.getArrived()); Serial.println(" ");
+  // Debug print statements
+  if (playback_DEBUG) {
+    Serial.print(base.getArrived()); Serial.print(" ");
+    Serial.print(shoulder.getArrived()); Serial.print(" ");
+    Serial.print(elbow.getArrived()); Serial.print(" ");
+    Serial.print(wrist.getArrived()); Serial.print(" ");
+    Serial.print(rotation.getArrived()); Serial.print(" ");
+    Serial.print(gripper.getArrived()); Serial.println(" ");
   }
   if (
     base.getArrived() && 
@@ -305,155 +523,4 @@ boolean isArrived() {
   } else {
     return false;
   }
-}
-
-// Cycles through saved arm positions
-void playback() {
-  // Skips method without this loop
-  while (digitalRead(PLAYPAUSE) == HIGH) {
-  }
-  
-  // Count number of arm positions
-  actualFrames = 0;
-  for (int i = 0; i < frames; i++) {
-    if (positionArray[i][0] != 0) {
-      actualFrames++;
-    }
-  }
-
-  // Initializes the integral as the pwm output for where the servo already is
-  preInitializer();
-  // Connects all servos
-  attach();
-  boolean playing = true;
-  // Each for() loop represents one "move" to a arm position
-  for (int i = 0; i < actualFrames; i++) {
-    // Initializes the arrival conditions as false before each move
-    loopInitializer();
-    // While the arrival conditions are false, calculates error correction output for each servo
-    while(!isArrived()) {
-      if (digitalRead(PLAYPAUSE) == HIGH) {
-        playing = false;
-        break;
-      }
-      computePath(i);      
-      delayMicroseconds(delay_us);
-    }
-    if (!playing) break;
-    Serial.print("Move "); Serial.print(i+1); Serial.print(" completed!"); Serial.print("\n");
-  }
-}
-
-
-
-// "Programs" the robot arm by saving an array of positions. Each position is a "frame."
-void program() {
-  // Will skip this method without this loop
-  while (digitalRead(PROGRAM) == HIGH) {
-  }
-  // Set all frames to 0
-  for (int i = 0; i < frames; i++) {
-    for (int j = 0; j < 2; j++) {
-      positionArray[i][j] = 0;
-    }
-  }
-  // Assigns values to the array in sequential order
-  boolean programming_mode = true;
-  detach();
-  for (int i = 0; i < frames; i++) {
-    //  Exits programming mode when the "Program" button is pressed again
-    if (programming_mode == false) {
-      break;
-    }
-
-    // Will skip without this loop
-    while (programming_mode && digitalRead(SAVE) == HIGH) {
-    }
-    delay(200);
-
-    //  Waits until "Save" or "Program" is pressed
-    bool waiting = true;
-    while (waiting) {
-      feedbackRefresh();
-//      Serial.print(base.getFeedback()); Serial.print(" ");
-//      Serial.print(shoulder.getFeedback()); Serial.print(" ");
-//      Serial.print(elbow.getFeedback()); Serial.print(" ");
-//      Serial.print(wrist.getFeedback()); Serial.print(" ");
-//      Serial.print(rotation.getFeedback()); Serial.print(" ");
-//      Serial.print(gripper.getFeedback()); Serial.println(" ");
-      // If "Program" is pressed while in programming mode, the positions are saved and programming mode is exited
-      if (digitalRead(PROGRAM) == HIGH) {
-        programming_mode = false;
-        waiting = false;
-      }
-
-      // If "Save" is pressed, feed from the servo is converted to microseconds for the same position, then saved
-      if (digitalRead(SAVE) == HIGH) {
-        while (digitalRead(PROGRAM)==HIGH) {}
-        //Serial.print("beep boop"); Serial.print("\n");
-        savePositions(i);
-        Serial.print("Saved!\n");
-        waiting = false;
-      }
-    }
-  }
-
-  Serial.print("Program Saved!\n");
-  int baseF = base.getFeedback();
-  int shoulderF = shoulder.getFeedback();
-  int elbowF = elbow.getFeedback();
-  int wristF = wrist.getFeedback();
-  int rotationF = rotation.getFeedback();
-  int gripperF = gripper.getFeedback();
-  preInitializer();
-  loopInitializer();
-  while(!isArrived()) {
-      base.computePath(baseF);
-      shoulder.computePath(shoulderF);
-      elbow.computePath(elbowF);
-      wrist.computePath(wristF);
-      rotation.computePath(rotationF);
-      gripper.computePath(gripperF);
-      delayMicroseconds(delay_us);
-  }
-  // Count number of frames
-  float actualFrames = 0;
-  for (int i = 0; i < frames; i++) {
-    if (positionArray[i][0] != 0) {
-      actualFrames++;
-    }
-  }
-
-//  attach();
-//  while(!isArrived()) {
-//      computePath(actualFrames-1);
-//      delay(delay_ms);
-//  }
-
-  // Prints saved positions
-  for (int i = 0; i < actualFrames; i++) {
-    Serial.print("Position "); Serial.print(i + 1); 
-    Serial.print(" feedback: "); 
-    Serial.print(positionArray[i][0]); Serial.print(" "); 
-    Serial.print(positionArray[i][1]); Serial.print(" "); 
-    Serial.print(positionArray[i][2]); Serial.print(" "); 
-    Serial.print(positionArray[i][3]); Serial.print(" ");
-    Serial.print(positionArray[i][4]); Serial.print(" ");
-    Serial.println(positionArray[i][5]);
-  }
-}
-
-
-float rangeMap(float a, float b, float c, float d, float input) {
-  // Takes an input between a and b and linearly maps the output between c and d
-  float x = 0;
-  if (input > b) {
-    x = b;
-  } else if (input < a) {
-    x = a;
-  } else {
-    x = input;
-  }
-  float value = (c * (1 - ((x - a) / (b - a)))) + (d * ((x - a) / (b - a)));
-  return value;
 }
